2462dfe6e2fcbc13706498cd7d061591143ea7a0
MilkuriNandhitha <2303a51352@sru.edu.in>
2026-03-26 11:46:05 +0530

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { clearOldData } from "./lib/clearOldData";
-
-// Clear all old demo/test data on first load
-clearOldData();
-
-createRoot(document.getElementById("root")!).render(<App />);
